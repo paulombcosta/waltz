1aaf76035ec07a2d947f2f5a1ce3159991c65950
Paulo Costa <paulo.costa@zalando.de>
2023-02-21 22:47:00 +0100

    WIP

--- go.mod
+++ go.mod
@@ -5,17 +5,33 @@ go 1.19
 // replace github.com/paulombcosta/waltz/spotifyauth => ./spotifyauth
 
 require (
+	cloud.google.com/go/compute v1.18.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	github.com/dghubble/gologin v2.1.0+incompatible // indirect
 	github.com/dghubble/sessions v0.4.0 // indirect
 	github.com/go-chi/chi v1.5.4 // indirect
 	github.com/go-chi/chi/v5 v5.0.8 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
+	github.com/googleapis/gax-go/v2 v2.7.0 // indirect
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/mux v1.6.2 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/gorilla/sessions v1.2.1 // indirect
+	github.com/markbates/goth v1.76.0 // indirect
 	github.com/zmb3/spotify v1.3.0 // indirect
 	github.com/zmb3/spotify/v2 v2.3.1 // indirect
-	golang.org/x/net v0.6.0 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/oauth2 v0.5.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+	google.golang.org/api v0.110.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
+	google.golang.org/genproto v0.0.0-20230221151758-ace64dc21148 // indirect
+	google.golang.org/grpc v1.53.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
--- main.go
+++ main.go
@@ -6,21 +6,29 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/gorilla/sessions"
+	"github.com/go-chi/cors"
+	"google.golang.org/api/option"
+	"google.golang.org/api/youtube/v3"
 
-	// "github.com/paulombcosta/waltz/spotifyauth"
+	"github.com/gorilla/sessions"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/google"
+
 	spotify "github.com/zmb3/spotify/v2"
 	spotifyauth "github.com/zmb3/spotify/v2/auth"
 	"golang.org/x/oauth2"
 )
 
 const (
-	SPOTIFY_TOKEN_SESSION_KEY = "spotify-token"
-	redirectURI               = "http://localhost:8080/callback"
-	SESSION_NAME              = "token-session"
+	SPOTIFY_TOKEN_SESSION_KEY     = "spotify-token"
+	GOOGLE_USER_TOKEN_SESSION_KEY = "google-user"
+	redirectURI                   = "http://localhost:8080/callback"
+	SESSION_NAME                  = "token-session"
 )
 
 var (
@@ -40,12 +48,135 @@ type PageState struct {
 }
 
 func main() {
+	gothic.Store = store
+
+	goth.UseProviders(
+		google.New(
+			os.Getenv("GOOGLE_CLIENT_ID"),
+			os.Getenv("GOOGLE_CLIENT_SECRET"),
+			"http://localhost:8080/callback/google", "https://www.googleapis.com/auth/youtube"),
+	)
+
 	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
 	router.Get("/", http.HandlerFunc(homepageHandler))
 	router.Get("/spotify/login", http.HandlerFunc(spotifyLoginHandler))
-	router.Handle("/callback", http.HandlerFunc(completeAuth))
+	// router.Handle("/callback", http.HandlerFunc(completeAuth))
+	router.Handle("/auth/{provider}", http.HandlerFunc(startGoogleAuth))
+	router.Handle("/callback/google", http.HandlerFunc(googleAuthCallback))
 	log.Println("starting server on :8080")
 	http.ListenAndServe(":8080", router)
+}
+
+func getUser() {
+	// session, err := gothic.Store.Get(r, gothic.SessionName)
+	// if err != nil {
+	// 	log.Println("err :", err.Error())
+	// 	return
+	// }
+	// log.Println("session, ", session)
+	// log.Println("session values, ", session.Values)
+	// _, err = goth.GetProvider(session.Values["google"].(string))
+	// if err != nil {
+	// 	log.Println("err :", err.Error())
+	// 	return
+	// }
+	// sess, err := provider.UnmarshalSession(session.Values[gothic.SessionName].(string))
+	// if err != nil {
+	// 	log.Println("err :", err.Error())
+	// 	return
+	// }
+	// user, err := provider.FetchUser(sess)
+	// if err != nil {
+	// 	log.Println("err :", err.Error())
+	// 	return
+	// }
+	// log.Println("user: ", user)
+
+}
+
+func startGoogleAuth(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, SESSION_NAME)
+	usr := session.Values[GOOGLE_USER_TOKEN_SESSION_KEY]
+	if usr != nil {
+		log.Println("user present")
+		source := TokenSource{User: usr.(goth.User)}
+		youtubeService, err := youtube.NewService(context.Background(), option.WithTokenSource(source))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		call := youtubeService.Playlists.List([]string{})
+		res, err := call.Do()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		log.Println("result ", res)
+		log.Printf("playlist response = %v", res)
+	} else {
+		gothic.BeginAuthHandler(w, r)
+	}
+}
+
+func googleAuthCallback(w http.ResponseWriter, r *http.Request) {
+	// session, err := gothic.Store.Get(r, gothic.SessionName)
+	// if err != nil {
+	// 	log.Println("err :", err.Error())
+	// 	return
+	// }
+	// // log.Println("session, ", session)
+	// log.Println("session values, ", session.Values)
+	// provider, err := goth.GetProvider("google")
+	// if err != nil {
+	// 	log.Println("err :", err.Error())
+	// 	return
+	// }
+	// log.Println("provider ", provider)
+	// sess, err := provider.UnmarshalSession(session.Values["google"].(string))
+	// if err != nil {
+	// 	log.Println("err :", err.Error())
+	// 	return
+	// }
+	// log.Println("session ", sess)
+	// user, err := provider.FetchUser(sess)
+	// if err != nil {
+	// 	log.Println("err :", err.Error())
+	// 	return
+	// }
+	// log.Println("user: ", user)
+
+	log.Println("CALLBACK WAS CALLED")
+	user, err := gothic.CompleteUserAuth(w, r)
+	log.Println("AFTER COMPLETE AUTH")
+	log.Println("user, ", user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	log.Println("access token: ", user.AccessToken)
+	session, _ := store.Get(r, SESSION_NAME)
+	session.Values[GOOGLE_USER_TOKEN_SESSION_KEY] = user
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+type TokenSource struct {
+	User goth.User
+}
+
+func (s TokenSource) Token() (*oauth2.Token, error) {
+	return &oauth2.Token{
+		AccessToken:  s.User.AccessToken,
+		RefreshToken: s.User.RefreshToken,
+	}, nil
 }
 
 func loadHomeTemplate() (*template.Template, error) {
