853fd5d1d5317b5246820453f68b8ebf08c3704f
Paulo Costa <paulombcosta@gmail.com>
2023-02-23 03:37:38 -0200

    WIP

--- handlers.go
+++ handlers.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"html/template"
 	"log"
@@ -12,36 +13,135 @@ import (
 	"github.com/markbates/goth"
 	"github.com/markbates/goth/gothic"
 	spotify "github.com/zmb3/spotify/v2"
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
 	"golang.org/x/oauth2"
-	"google.golang.org/api/option"
-	"google.golang.org/api/youtube/v3"
 )
 
-type AuthData struct {
+// func (a application) startGoogleAuth(w http.ResponseWriter, r *http.Request) {
+// 	session, _ := a.store.Get(r, SESSION_NAME)
+// 	tokens := session.Values[GOOGLE_USER_TOKEN_SESSION_KEY]
+// 	log.Println("tokens", tokens)
+// 	if tokens != nil {
+// 		log.Println("user present, refreshing token")
+// 		provider, err := goth.GetProvider("google")
+// 		if err != nil {
+// 			http.Error(w, fmt.Sprintf("unable to get google provider due to: %s", err.Error()), http.StatusInternalServerError)
+// 			return
+// 		}
+
+// 		t := tokens.(oauth2.Token)
+
+// 		log.Println("user acccess token", t.AccessToken)
+// 		log.Println("user refresh token", t.RefreshToken)
+
+// 		updatedToken, err := provider.RefreshToken(t.RefreshToken)
+
+// 		if err != nil {
+// 			http.Error(w, fmt.Sprintf("unable to get google provider due to: %s", err.Error()), http.StatusInternalServerError)
+// 			return
+// 		}
+
+// 		session.Values[GOOGLE_USER_TOKEN_SESSION_KEY] = oauth2.Token{AccessToken: updatedToken.AccessToken, RefreshToken: updatedToken.RefreshToken}
+// 		session.Save(r, w)
+
+// 		source := TokenSource{Source: *updatedToken}
+
+//			youtubeService, err := youtube.NewService(context.Background(), option.WithTokenSource(source))
+//			if err != nil {
+//				http.Error(w, err.Error(), http.StatusInternalServerError)
+//				return
+//			}
+//			call := youtubeService.Playlists.List([]string{"snippet", "id", "contentDetails"})
+//			call.Mine(true)
+//			res, err := call.Do()
+//			if err != nil {
+//				http.Error(w, err.Error(), http.StatusInternalServerError)
+//				return
+//			}
+//			for _, p := range res.Items {
+//				log.Println("playlist title: ", p.Snippet.Title)
+//				log.Println("playlist kind: ", p.Kind)
+//			}
+//			log.Println("result ", res)
+//			log.Printf("playlist response = %v", res)
+//		} else {
+//			gothic.BeginAuthHandler(w, r)
+//		}
+//	}
+func getSessionTokens(provider string, r *http.Request, store *sessions.CookieStore) (*oauth2.Token, error) {
+	session, err := store.Get(r, SESSION_NAME)
+	if err != nil {
+		return nil, err
+	}
+	if provider == "google" {
+		return session.Values[GOOGLE_USER_TOKEN_SESSION_KEY].(*oauth2.Token), nil
+	} else if provider == "spotify" {
+		return session.Values[SPOTIFY_TOKEN_SESSION_KEY].(*oauth2.Token), nil
+	} else {
+		return nil, errors.New(fmt.Sprintf("invalid provider %s", provider))
+	}
 }
 
-func (a application) spotifyLoginHandler(w http.ResponseWriter, r *http.Request) {
-	url := auth.AuthURL(state)
-	w.Header().Set("HX-Redirect", url)
+func updateSession(session *sessions.Session, provider string, tokens *oauth2.Token, r *http.Request, w http.ResponseWriter) error {
+	if provider == "spotify" {
+		session.Values[SPOTIFY_TOKEN_SESSION_KEY] = tokens
+	} else if provider == "google" {
+		session.Values[GOOGLE_USER_TOKEN_SESSION_KEY] = tokens
+	} else {
+		return errors.New(fmt.Sprintf("invalid provider %s", provider))
+	}
+	return session.Save(r, w)
+}
+
+func refreshToken(
+	providerName string,
+	r *http.Request,
+	w http.ResponseWriter,
+	store *sessions.CookieStore) (*oauth2.Token, error) {
+
+	session, err := store.Get(r, SESSION_NAME)
+	if err != nil {
+		return nil, err
+	}
+	provider, err := goth.GetProvider(providerName)
+	if err != nil {
+		return nil, err
+	}
+	existingTokens, err := getSessionTokens(providerName, r, store)
+	if err != nil {
+		return nil, err
+	}
+	newTokens, err := provider.RefreshToken(existingTokens.RefreshToken)
+	if err != nil {
+		return nil, err
+	}
+	err = updateSession(session, providerName, newTokens, r, w)
+	if err != nil {
+		return nil, err
+	}
+	return newTokens, nil
 }
 
 func (a application) homepageHandler(w http.ResponseWriter, r *http.Request) {
 	pageState := PageState{LoggedInSpotify: false, LoggedInYoutube: false}
-	spotifyClient := getSpotifyClient(r, a.store)
+	spotifyClient, err := getSpotifyClient(r, w, a.store)
+	if err != nil {
+		log.Println("error getting spotify client: ", err.Error())
+	}
 	if spotifyClient != nil {
 		log.Println("spotify client has been initialized")
 		user, err := client.CurrentUser(context.Background())
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+			log.Println("error getting current user: ", err.Error())
+		} else {
+			pageState.LoggedInSpotify = true
+			pageState.SpotifyUser = user.ID
 		}
-		pageState.LoggedInSpotify = true
-		pageState.SpotifyUser = user.ID
 	} else {
 		log.Println("token not found, user not logged in")
 	}
 	tmpl := template.Must(loadHomeTemplate())
-	err := tmpl.Execute(w, pageState)
+	err = tmpl.Execute(w, pageState)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -53,108 +153,47 @@ func loadHomeTemplate() (*template.Template, error) {
 	return template.New(filepath.Base(name)).ParseFiles(name)
 }
 
-func getSpotifyClient(r *http.Request, store *sessions.CookieStore) *spotify.Client {
-	if client != nil {
-		return client
-	} else {
-		session, _ := store.Get(r, SESSION_NAME)
-		tok := session.Values[SPOTIFY_TOKEN_SESSION_KEY]
-		if tok != nil {
-			client = spotify.New(auth.Client(r.Context(), tok.(*oauth2.Token)))
-			return client
-		} else {
-			return nil
+func getSpotifyClient(r *http.Request, w http.ResponseWriter, store *sessions.CookieStore) (*spotify.Client, error) {
+	session, _ := store.Get(r, SESSION_NAME)
+	tok := session.Values[SPOTIFY_TOKEN_SESSION_KEY]
+	log.Println("saved token = ", tok)
+	if tok != nil {
+		newTokens, err := refreshToken("spotify", r, w, store)
+		if err != nil {
+			return nil, err
 		}
+		client = spotify.New(spotifyauth.New().Client(r.Context(), newTokens))
+		return client, nil
+	} else {
+		return nil, nil
 	}
 }
 
-func (a application) spotifyAuthCallback(w http.ResponseWriter, r *http.Request) {
-	tok, err := auth.Token(r.Context(), state, r)
-	if err != nil {
-		http.Error(w, "Couldn't get token", http.StatusForbidden)
-		log.Fatal(err)
-	}
-	if st := r.FormValue("state"); st != state {
-		http.NotFound(w, r)
-		log.Fatalf("State mismatch: %s != %s\n", st, state)
-	}
+func (a application) authCallbackHandler(w http.ResponseWriter, r *http.Request) {
 
-	log.Println("token:", tok)
-	session, _ := a.store.Get(r, SESSION_NAME)
-	// only store the bare-minimum necessary
-	session.Values[SPOTIFY_TOKEN_SESSION_KEY] = oauth2.Token{AccessToken: tok.AccessToken, RefreshToken: tok.RefreshToken}
-	err = session.Save(r, w)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	provider := r.URL.Query().Get("provider")
+	if provider == "" {
+		http.Error(w, "provider was not specified", http.StatusInternalServerError)
 		return
 	}
-	http.Redirect(w, r, "/", http.StatusSeeOther)
-}
 
-func (a application) startGoogleAuth(w http.ResponseWriter, r *http.Request) {
-	session, _ := a.store.Get(r, SESSION_NAME)
-	tokens := session.Values[GOOGLE_USER_TOKEN_SESSION_KEY]
-	log.Println("tokens", tokens)
-	if tokens != nil {
-		log.Println("user present, refreshing token")
-		provider, err := goth.GetProvider("google")
-		if err != nil {
-			http.Error(w, fmt.Sprintf("unable to get google provider due to: %s", err.Error()), http.StatusInternalServerError)
-			return
-		}
-
-		t := tokens.(oauth2.Token)
-
-		log.Println("user acccess token", t.AccessToken)
-		log.Println("user refresh token", t.RefreshToken)
-
-		updatedToken, err := provider.RefreshToken(t.RefreshToken)
-
-		if err != nil {
-			http.Error(w, fmt.Sprintf("unable to get google provider due to: %s", err.Error()), http.StatusInternalServerError)
-			return
-		}
-
-		session.Values[GOOGLE_USER_TOKEN_SESSION_KEY] = oauth2.Token{AccessToken: updatedToken.AccessToken, RefreshToken: updatedToken.RefreshToken}
-		session.Save(r, w)
-
-		source := TokenSource{Source: *updatedToken}
-
-		youtubeService, err := youtube.NewService(context.Background(), option.WithTokenSource(source))
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		call := youtubeService.Playlists.List([]string{"snippet", "id", "contentDetails"})
-		call.Mine(true)
-		res, err := call.Do()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		for _, p := range res.Items {
-			log.Println("playlist title: ", p.Snippet.Title)
-			log.Println("playlist kind: ", p.Kind)
-		}
-		log.Println("result ", res)
-		log.Printf("playlist response = %v", res)
-	} else {
-		gothic.BeginAuthHandler(w, r)
-	}
-}
-
-func (a application) googleAuthCallback(w http.ResponseWriter, r *http.Request) {
 	user, err := gothic.CompleteUserAuth(w, r)
-	log.Println("AFTER COMPLETE AUTH")
-	log.Println("user acess token, ", user.AccessToken)
-	log.Println("user refresh token, ", user.RefreshToken)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	session, _ := a.store.Get(r, SESSION_NAME)
-	session.Values[GOOGLE_USER_TOKEN_SESSION_KEY] = oauth2.Token{AccessToken: user.AccessToken, RefreshToken: user.RefreshToken}
-	session.Save(r, w)
+
+	tokens := oauth2.Token{AccessToken: user.AccessToken, RefreshToken: user.RefreshToken}
+	log.Println("access token: ", tokens.AccessToken)
+	log.Println("refresh token: ", tokens.RefreshToken)
+	err = updateSession(session, provider, &tokens, r, w)
+	log.Println("session updated")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
--- main.go
+++ main.go
@@ -12,9 +12,9 @@ import (
 	"github.com/markbates/goth"
 	"github.com/markbates/goth/gothic"
 	"github.com/markbates/goth/providers/google"
+	spotifyProvider "github.com/markbates/goth/providers/spotify"
 
 	spotify "github.com/zmb3/spotify/v2"
-	spotifyauth "github.com/zmb3/spotify/v2/auth"
 	"golang.org/x/oauth2"
 )
 
@@ -27,8 +27,6 @@ const (
 
 var (
 	// TODO get a proper session key
-	auth = spotifyauth.New(spotifyauth.WithRedirectURL(redirectURI),
-		spotifyauth.WithScopes(spotifyauth.ScopeUserReadPrivate, spotifyauth.ScopePlaylistReadPrivate))
 	// TODO set a proper state
 	state  = "abc123"
 	client *spotify.Client
@@ -46,7 +44,12 @@ func main() {
 		google.New(
 			os.Getenv("GOOGLE_CLIENT_ID"),
 			os.Getenv("GOOGLE_CLIENT_SECRET"),
-			"http://localhost:8080/callback/google", "email", "https://www.googleapis.com/auth/youtube"),
+			"http://localhost:8080/auth/callback?provider=google", "email", "https://www.googleapis.com/auth/youtube"),
+		spotifyProvider.New(
+			os.Getenv("SPOTIFY_ID"),
+			os.Getenv("SPOTIFY_SECRET"),
+			"http://localhost:8080/auth/callback?provider=spotify",
+			"user-read-private", "playlist-read-private"),
 	)
 
 	router := chi.NewRouter()
@@ -54,10 +57,10 @@ func main() {
 	app := application{store: store}
 
 	router.Get("/", http.HandlerFunc(app.homepageHandler))
-	router.Get("/spotify/login", http.HandlerFunc(app.spotifyLoginHandler))
-	router.Handle("/callback", http.HandlerFunc(app.spotifyAuthCallback))
-	router.Handle("/auth/{provider}", http.HandlerFunc(app.startGoogleAuth))
-	router.Handle("/callback/google", http.HandlerFunc(app.googleAuthCallback))
+	// router.Get("/auth", http.HandlerFunc(app.spotifyLoginHandler))
+	// router.Handle("/auth", http.HandlerFunc(app.startGoogleAuth))
+	router.Get("/auth", gothic.BeginAuthHandler)
+	router.Handle("/auth/callback", http.HandlerFunc(app.authCallbackHandler))
 	log.Println("starting server on :8080")
 	http.ListenAndServe(":8080", router)
 }
